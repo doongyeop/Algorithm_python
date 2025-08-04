ecc25f84a138d247d348de8e40402099e5321a29
yeop <leedy977@gmail.com>
2025-08-05 00:48:42 +0900

    [Silver III] Title: N과 M (7), Time: 480 ms, Memory: 112672 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,9 @@
+from itertools import product as p
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+result = p(nums, repeat=m)
+
+for r in result:
+    print(" ".join(map(str, r)))
