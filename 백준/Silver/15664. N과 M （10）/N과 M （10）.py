77afb80a1f864456c18bc10a7d41a002e5f312c8
yeop <leedy977@gmail.com>
2025-08-05 00:56:44 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,9 @@
+from itertools import combinations as c
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+result = sorted(list(set(c(nums, m))))
+
+for r in result:
+    print(" ".join(map(str, r)))
