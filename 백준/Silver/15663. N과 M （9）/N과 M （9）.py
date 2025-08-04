6f0cadd8a8105ae9809ce4cfb8810f19d49466b6
yeop <leedy977@gmail.com>
2025-08-05 00:54:04 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 114496 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,9 @@
+from itertools import permutations as p
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+result = sorted(list(set(p(nums, m))))
+
+for r in result:
+    print(" ".join(map(str, r)))
