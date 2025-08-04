d841ee099deb52cea7519745b30f98ff2ea34aa6
yeop <leedy977@gmail.com>
2025-08-05 00:50:14 +0900

    [Silver III] Title: N과 M (8), Time: 1804 ms, Memory: 112264 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,10 @@
+from itertools import product as p
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+result = p(nums, repeat=m)
+
+for r in result:
+    if all(r[i] <= r[i + 1] for i in range(len(r) - 1)):
+        print(" ".join(map(str, r)))
