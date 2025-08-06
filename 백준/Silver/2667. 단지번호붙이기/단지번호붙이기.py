0f1e8a7e2cdec368427bb92e77c419996483d119
yeop <leedy977@gmail.com>
2025-08-07 00:57:18 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 109708 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,43 @@
+n = int(input())
+
+maps = [[0] * n for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+for i in range(n):
+    maps[i] = list(map(int, input()))
+# print(maps)
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+cnt = 1
+ans = []
+
+
+def dfs(i, j):
+    global cnt
+
+    visited[i][j] = True
+
+    for d in range(4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+
+        if not (0 <= nx < n and 0 <= ny < n): continue
+        if visited[nx][ny] or maps[nx][ny] == 0: continue
+
+        cnt += 1
+        dfs(nx, ny)
+
+
+for i in range(n):
+    for j in range(n):
+        if maps[i][j] != 0 and not visited[i][j]:
+            dfs(i, j)
+            ans.append(cnt)
+            cnt = 1
+
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)
