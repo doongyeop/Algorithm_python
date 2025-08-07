82550a9b7ed36ac809bc808f0865a94eed33a0a4
yeop <leedy977@gmail.com>
2025-08-07 12:44:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,42 +1,28 @@
+import sys
+
+input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int, input().split())
+MAX = 100_001
+row = [0] * MAX
 
-row = [0] * 100_001
 
-
-def bfs(x):
+def bfs():
     q = deque()
-    q.append(x)
-    row[x] = 1
+    q.append(n)
 
     while q:
-        t = q.popleft()
+        curr = q.popleft()
 
-        n1 = t - 1
-        n2 = t + 1
-        n3 = t * 2
+        if curr == k:
+            print(row[curr])
+            break
 
-        if in_boundary(n1) and row[n1] == 0:
-            q.append(n1)
-            row[n1] = row[t] + 1
-
-        if in_boundary(n2) and row[n2] == 0:
-            q.append(n2)
-            row[n2] = row[t] + 1
-
-        if in_boundary(n3) and row[n3] == 0:
-            q.append(n3)
-            row[n3] = row[t] + 1
-
-        if n1 == k or n2 == k or n3 == k:
-            return row[k]
-        # print(row[0:18:1])
-    return -1
+        for next in (curr - 1, curr + 1, curr * 2):
+            if 0 <= next < MAX and row[next] == 0:
+                row[next] = row[curr] + 1
+                q.append(next)
 
 
-def in_boundary(x):
-    return 0 <= x <= 100_000
-
-
-print(bfs(n) - 1)
+bfs()
