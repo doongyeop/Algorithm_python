2d4bc0802313b7fbf97a63f8e2651fdafee2b8cd
yeop <leedy977@gmail.com>
2025-08-10 01:42:49 +0900

    [level 3] Title: 정수 삼각형, Time: 50.46 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    n = len(triangle)
+    dp = [[0] * len(row) for row in triangle]
+
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, n):
+        for j in range(len(triangle[i])):
+            if j == 0: 
+                dp[i][j] = triangle[i][j] + dp[i - 1][j]
+            elif j == len(triangle[i]) - 1: 
+                dp[i][j] = triangle[i][j] + dp[i - 1][j - 1]
+            else: 
+                dp[i][j] = triangle[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j])
+
+    return max(dp[-1])
